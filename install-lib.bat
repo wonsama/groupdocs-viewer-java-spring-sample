88f6e02ac1944288496762e05f27a8fc9feffd7f
Alex Bobkov <lilalex85@gmail.com>
2014-01-16 16:53:09 +0200

    minor fixes

--- install-lib.bat
+++ install-lib.bat
@@ -1,2 +1,2 @@
-call mvn install:install-file -Dpackaging=jar -Dversion=1.5.1    -DgroupId=com.groupdocsviewer -DartifactId=GroupDocsJavaViewer    -Dfile=lib/GroupDocsJavaViewer-1.5.1.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=1.6.5    -DgroupId=com.groupdocsviewer -DartifactId=GroupDocsJavaViewer    -Dfile=lib/GroupDocsJavaViewer-1.6.5.jar
 PAUSE
