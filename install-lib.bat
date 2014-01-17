19522a174ab33e5f196b3c65bb0f08ed06824987
Alex Bobkov <lilalex85@gmail.com>
2014-01-17 12:50:34 +0200

    lib updated

--- install-lib.bat
+++ install-lib.bat
@@ -1,2 +1,2 @@
-call mvn install:install-file -Dpackaging=jar -Dversion=1.6.5    -DgroupId=com.groupdocs -DartifactId=viewer    -Dfile=lib/GroupDocsJavaViewer-1.6.5.jar
+call mvn install:install-file -Dpackaging=jar -Dversion=1.6.5    -DgroupId=com.groupdocs        -DartifactId=viewer  -Dfile=lib/GroupDocsJavaViewer-1.6.5.jar
 PAUSE
